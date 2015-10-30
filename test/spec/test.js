713581456d70ab3a16690d8cb4ad2e990cbe2313
Mateusz Labusek <mateusz.labusek@gmail.com>
2015-10-30 14:26:45 +0100

    first tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,10 +1,31 @@
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-
+  describe('jQuery call', function () {
+    describe('with class', function () {
+      var myEl, otherEl;
+      beforeEach(function() {
+        otherEl = document.createElement( 'div' );
+        otherEl.className = 'other-class';
+        myEl = document.createElement( 'div' );
+        myEl.className = 'my-class';
+      });
+      it('should return an object', function () {
+        var node = $('my-class');
+        expect(node).toBeDefined();
+      });
+      it('should have methods', function () {
+        var node = $('my-class');
+        expect(node.addClass).toBeDefined();
+        expect(node.append).toBeDefined();
+        expect(node.html).toBeDefined();
+        expect(node.attr).toBeDefined();
+        expect(node.children).toBeDefined();
+        expect(node.css).toBeDefined();
+        expect(node.data).toBeDefined();
+        expect(node.on).toBeDefined();
+        expect(node.one).toBeDefined();
+        expect(node.each).toBeDefined();
       });
     });
   });
